072166137a44b82fd6584b742024c57cde59b003
wexng <wenxg@si-tech.com.cn>
2015-11-23 15:15:57 +0800

    xiugai ceshi

---
+++ gitDemo/src/TestJava.java
@@ -0,0 +1,13 @@
+
+public class TestJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.print("wenxiaogen");
+		  System.out.print("wenxiaogen1");
+	}
+
+}
